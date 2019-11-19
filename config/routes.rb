d1d4e16bf596a1cce5f34c8f3dac2afefb946ade
Lionel Melin <lionel.melin@gmail.com>
2019-11-19 11:36:53 +0100

    controller user, dashboard for users#show, db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :friends do
+  get 'users/show'
+  get 'users/index'
+  resources :friends, except: [:show] do
     resources :bookings, only: [:new, :create]
   end
   devise_for :users
-  root to: 'pages#home'
+  get '/dashboard', to: 'users#show', as: :dashboard
+  root to: 'friends#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_162505) do
+ActiveRecord::Schema.define(version: 2019_11_19_100610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_11_18_162505) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 2019_11_18_162505) do
 
   add_foreign_key "bookings", "friends"
   add_foreign_key "bookings", "users"
+  add_foreign_key "friends", "users"
 end
