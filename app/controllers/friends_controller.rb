e41fe15d2250f9bcecd87be317a4c4be8f66b6e5
Pierre Nicoletti <pierre.nicoletti@gmail.com>
2019-11-19 17:39:16 +0100

    auth skipped for index

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,5 @@
 class FriendsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_friend, only: [:show, :edit, :update, :destroy]
   def new
     @friend = current_user.friends.new
