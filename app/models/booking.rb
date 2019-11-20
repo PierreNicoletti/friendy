d6815af29e67c69b7d0588a4ac44099d3c9fcc7d
Laura Carrere <laura.carrere@gmail.com>
2019-11-20 12:50:02 +0100

    booking.rb changed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,24 @@
 class Booking < ApplicationRecord
   belongs_to :friend
   belongs_to :user
+
+  def display_date(date)
+    month = date.month
+    day = date.day
+    w_day = date.wday
+    year = date.year
+    return "#{explicit_day(w_day)} #{day} #{explicit_month(month)} #{year}"
+  end
+
+  private
+
+  def explicit_month(month)
+    months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    return months[month - 1]
+  end
+
+  def explicit_day(day)
+    days = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+    return days[day]
+  end
 end
