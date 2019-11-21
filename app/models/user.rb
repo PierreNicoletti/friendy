799c0715808990df9cdfd5881d13f5296b48d86b
Lionel Melin <lionel.melin@gmail.com>
2019-11-21 12:40:29 +0100

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings
   has_many :friends
+  has_one_attached :photo
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
+  def age
+    return if birth_date.nil?
+    (Date.today - birth_date.to_date).abs.round / 365
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_104120) do
+ActiveRecord::Schema.define(version: 2019_11_21_110728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_11_20_104120) do
   create_table "friends", force: :cascade do |t|
     t.string "name"
     t.string "gender"
-    t.string "city"
     t.text "description"
     t.decimal "price"
     t.datetime "created_at", null: false
@@ -59,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_104120) do
     t.date "birth_date"
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 2019_11_20_104120) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "gender"
+    t.date "birth_date"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
